d2015ba3f3ddfe1e5b2c65204297849fe6f06998
yuchi.zhou <yuchi.zhou@zhaopin.com.cn>
2025-09-08 18:24:59 +0800

    test

--- src/extract_label.py
+++ src/extract_label.py
@@ -6,34 +6,44 @@ import re
 import sys
 import requests
 
+
 def llm_interface(messages):
-    url = 'http://172.16.1.151:8000/v1/chat/completions'
-    headers = {"Content-Type": "application/json"}
+    sdkey = "Bearer sk-9920838060a4455184ef7a433135b06c"
+    url = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation'
+    headers = {"Authorization": sdkey, "Content-Type": "application/json"}
     params = {
-        "model": "/data/model/qwen3-14b",
-        "messages": messages,
-        "temperature": 0.6, "stream": False, "chat_template_kwargs": {"enable_thinking": False}
+        "model": "qwen-plus-latest",
+        "input":{"messages": messages},
+        "parameters": {"temperature": 0.6, "result_format": "message", "stream": False, "enable_thinking": False}
     }
-    try:
-        resp = requests.post(url, json=params, headers=headers)
+    resp = requests.post(url, json=params, headers=headers)
+    # print(resp.json())
+    if resp.status_code == 200:
         output_data = resp.json()
-        return output_data['choices'][0]['message']['content']
-    except Exception as e:
-        return ""
+        return output_data['output']['choices'][0]['message']['content']
+    return ""
 
 system_prompt = """# 任务
-你是一个地址信息提取助手。请从用户的多轮对话中识别并提取【家的地址】和【公司地址】
+你是一个严谨的信息提取助手。从用户多轮对话中识别并提取用户明确提及或确认的【家的地址】和【公司地址】
 
-# 要求：
-【家的地址】(home)：完整地包含家的地址的全部细节
-【公司地址】(comp)：完整地包含公司地址的全部细节
-例如：菊园2号楼4单元301
+# 提取要求
+1.明确归属：
+- 家的地址 (home): 必须与 "家"、"回家"、"我住的"、"小区"、"住址" 等词语直接绑定，或紧邻的上下文中直接关联
+- 公司地址 (comp)：必须与 "公司"、"上班"、"单位"、"办公室" 等词语直接绑定，或紧邻的上下文中直接关联
+2.禁止进行任何联想推断。例如，**绝对不能**因为行程的终点是“家”，就自动推断其起点是“公司”
+- 示例：用户说“从昊海大厦地出发回家”，昊海大厦不是“公司”
+3.排除无关地点：对话中仅作为临时出发地、目的地、途经点提到的地址，若没有上述明确的归属词语关联，则**一律忽略**，不得提取。
+4.**完整性**：提取的地址应包含所有细节。
+
+# 严格按照以下步骤执行
+## 第1步：对每行【多轮对话】数据，提取【家的地址】和【公司地址】
+## 第2步：合并结果
+ - 如果没有提取到某个地址，将其值设为空字符串。
+ - 如果存在多个值，用分号分隔。
 
 # 输出
 必须是严格的JSON格式，确保格式正确，不要增加任何字符
-如果没有提取到某个地址，将其值设为空字符串。
-如果存在多个值，用分号分隔。
-示例输出：{"home": "菊园2号楼4单元301;知春路2楼208", "comp": "方正大厦东南门"}"""
+示例：{"home": "菊园2号楼4单元301;知春路(公交站)", "comp": "昊海大厦(东南门)"}"""
 
 def str2json_llm_output(arg1):
     if isinstance(arg1, str):
@@ -43,16 +53,16 @@ def str2json_llm_output(arg1):
     else:
         return arg1
 
-def extract_address_batch(msgs, rslt):
+def extract_address_batch2(msgs, rslt):
     messages = [{"role": "system", "content": system_prompt}]
     for msg in msgs:
         role = "user" if msg.role == "user" else "assistant"
         messages.append({"role": role, "content": msg.msg})
+    print(messages)
 
     for i in range(3):
         try:
             resp = llm_interface(messages)
-            print(messages)
             print(resp)
             resp = str2json_llm_output(resp)
             field_map = {"home": "address_home", "comp": "address_company"}
@@ -64,13 +74,56 @@ def extract_address_batch(msgs, rslt):
             pass
     return rslt
 
+
+def extract_address_batch(msgs, rslt):
+    chat = "'\n'".join(msgs)
+    user_message = {"role": "user", "content": f"# 输入\n【多轮会话】：（每行数据表示一次多轮对话）\n'{chat}'"}
+    messages = [{"role": "system", "content": system_prompt}, user_message]
+    print(user_message)
+
+    for i in range(3):
+        try:
+            resp = llm_interface(messages)
+            print(resp)
+            resp = str2json_llm_output(resp)
+            field_map = {"home": "address_home", "comp": "address_company"}
+            for field_llm, field_name in field_map.items():
+                if resp.get(field_llm, ""):
+                    rslt[field_name] = f"{rslt[field_name]};{resp[field_llm]}" if rslt.get(field_name) else resp[field_llm]
+            return rslt
+        except Exception as e:
+            pass
+    return rslt
+
+def gen_messages(msgs):
+    ms = []
+    pre_conversation_id = "NULL"
+    m = ""
+    for msg in msgs:
+        new_conversation_id = msg.conversation_id
+        role = "用户" if msg.role == "user" else "你"
+        if pre_conversation_id != new_conversation_id:
+            if m: ms.append(m)
+            m = f"{role}:{msg.msg}"
+            pre_conversation_id = new_conversation_id
+        else:
+            m = f"{m}; {role}:{msg.msg}"
+    if m: ms.append(m)
+    return ms
+
 def proc_extract_address(msgs):
     rslt = {}
-    # 分批次处理消息（每次最多1000条）
-    MAX_BATCH = 1000
-    for i in range(0, len(msgs), MAX_BATCH):
-        batch_msgs = msgs[i:i+MAX_BATCH+10]
-        rslt = extract_address_batch(batch_msgs, rslt)
+    ms = gen_messages(msgs)
+    # 分批次处理消息（每次最多200条）
+    MAX_BATCH = 200
+    for i in range(0, len(ms), MAX_BATCH):
+        batch_ms = ms[i:i + MAX_BATCH]
+        rslt = extract_address_batch(batch_ms, rslt)
+    # # 分批次处理消息（每次最多1000条）
+    # MAX_BATCH = 1000
+    # for i in range(0, len(msgs), MAX_BATCH):
+    #     batch_msgs = msgs[i:i+MAX_BATCH+10]
+    #     rslt = extract_address_batch2(batch_msgs, rslt)
     return rslt if rslt else None
 
 def main(current_date):
@@ -91,13 +144,13 @@ def main(current_date):
     # 过滤并排序
     filtered_df = df.filter(col("msg_type") == "must") \
         .withColumn("intent_id", col("intent_id").cast("string")) \
+        .filter(col("intent_id") != "10007") \
         .withColumn("id", col("id").cast("int")) \
         .orderBy(col("id"))
-        # .filter(col("intent_id") != "10007") \
 
     # 按用户ID分组，收集消息历史
     grouped_df = filtered_df.groupBy("user_id", "intent_id") \
-        .agg(collect_list(struct(col("role").alias("role"), col("msg").alias("msg"))).alias("messages"))
+        .agg(collect_list(struct(col("role").alias("role"), col("msg").alias("msg"), col("conversation_id").alias("conversation_id"))).alias("messages"))
 
     # 提取地址信息
     df_output = grouped_df.withColumn("addresses", proc_extract_address_udf(col("messages"))) \
